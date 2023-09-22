06392682ea9cc38a9f74070bde0155b298a380df
Harris Irfan <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2023-09-22 08:45:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className=' content-visible'>Tailwind Plugins</h1>
+     <h1 className=' content-visible text-red'>Tailwind Plugins</h1>
     </div>
   );
 }
